7b9806bc4f5daab03a88093d9f70b157738058e9
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-27 14:17:27 -0700

    adding on click event

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ function getPhotos(){
         let photosHTML = ""
 
     for(let i=0; i<data.length; i++){
+        let id="photo"+(i+1)
         photosHTML += `
-        <div class="photo-item"><img class="photo" src="https://picsum.photos/id/${data[i].id}/100/100" alt="${data[i].title}"></div>`
+        <div class="photo-item"><img class="photo" id="${id}" src="https://picsum.photos/id/${data[i].id}/100/100" alt="${data[i].title}"></div>`
     }
     document.getElementById("photo-container").innerHTML = photosHTML
 
@@ -19,4 +20,15 @@ function getPhotos(){
 
 getPhotos()
 
+document.getElementById("photo-container").addEventListener("click", loadImage)
 
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img
+
+function loadImage(e){
+    console.log("clicked")
+    let clickedImageID = e.target.id
+    console.log(clickedImageID)
+
+    let img = document.getElementById(clickedImageID)
+    document.getElementById("my-photo").src = img.src
+}
