521c837745867bcfceab406b28e89ca74ca15143
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-04-27 11:02:11 -0700

    output photos to screen

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
-async function getPhotos(){
-    let response = await fetch("photos.json")
-    let photos = await response.json()
-    console.log(photos)
+let photos = []
+
+function getPhotos(){
+    fetch("photos.json")
+    .then(response => response.json())
+    .then(data=>{
+        console.log(data)
+
+        let photosHTML = ""
+
+    for(let i=0; i<data.length; i++){
+        photosHTML += `
+        <div class="photo"><img src="https://picsum.photos/id/${data[i].id}/100/100" alt="${data[i].title}"></div>`
+    }
+    document.getElementById("photo-container").innerHTML = photosHTML
+
+    })
 }
 
 getPhotos()
+
+
+function outputPhotos(){
+    // let photosHTML = ""
+    // for(let i=0; i<photos.length; i++){
+    //     photosHTML += `
+    //     <div class="photo" src="https://picsum.photos/id/${photos[i].id}/100/100" alt="${photos[i].title}"></div>`
+    // }
+    // document.getElementById("container").innerHTML = photosHTML
+
+   
+}
